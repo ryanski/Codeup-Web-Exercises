e89f6f5a2ed6bbf1758bb2071ba4b6a5c6c14a85
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2015-10-08 09:20:34 -0500

    iterating arrays & functions & more

--- public/js/external.js
+++ public/js/external.js
@@ -1,5 +1,32 @@
 
 console.log("Hello from External JS!");
 
+var raining = false;
+var foggy =false;
+var sunny =false;
+var cloudy =true;
 
+if(raining) {"Bring an umbrella brella brella ay ay"
 
+} else if (foggy) {
+	console.log("It is foggy");
+}else if (cloudy){
+
+} else if (sunny){
+
+} else {
+
+}
+var success = false;
+var message;
+
+if (success) {
+    message = "Operation was successful.";
+} else {
+    message = "Oops, something went wrong.";
+}
+
+// the above if/else can be re-written as:
+var message = (success) ? "Operation was successful." : "Oops, something went wrong.";
+
+console.log(message);
