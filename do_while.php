a59c06006844449dd3cb4dccaea7b65963158a7f
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2015-11-04 10:07:24 -0600

    while update and do_while exercise.

---
+++ do_while.php
@@ -0,0 +1,23 @@
+<?php
+$a=0;
+do {
+	echo "\$a is equal to $a\n";
+$a=$a+2;
+}
+while($a<=100);
+
+$a=100;
+do {
+	echo "\$a is equal to $a\n";
+$a=$a-5;
+}
+while($a>=-10);
+
+$a=2;
+do {
+	echo "\$a is equal to $a\n";
+$a=$a*$a;
+}
+while($a<=1000000);
+
+?>
