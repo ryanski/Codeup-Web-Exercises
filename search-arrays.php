d022e76639e15cfbe9e291eb4174bae119729ffe
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2015-11-09 13:44:44 -0600

    learned how to search arrays.

---
+++ search-arrays.php
@@ -0,0 +1,41 @@
+<?php
+
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+// function search_a($person, $names){
+// 	$result=array_search($person, $names);
+// 	var_dump($result);
+// if($result===false){
+// 	echo "false\n";
+// 	} else {
+// 		echo "true\n";
+// 	}
+
+// }
+
+
+function isInarray ($person, $array)
+{
+	if(array_search($person, $array)!==false){
+		return true;
+	} return false;
+}
+
+$result=isInarray('Tinas', $names);
+var_dump($result);
+
+
+$i=0;
+foreach($names as $name){
+	foreach($compare as $each){
+		if($name===$each){
+			$i++;
+		}
+	} 
+	 
+} 
+echo $i.PHP_EOL;
+	
+
