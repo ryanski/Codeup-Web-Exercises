c5f40bc770ecfeeae98657dd3a6aabab17ddd90a
Ryan Bentkowski <RyanBentkowski@Macintosh-58b035f526c4.local>
2015-11-16 13:33:30 -0600

    created pong and ping php files.  learn about post.

---
+++ public/ping.php
@@ -0,0 +1,29 @@
+<?php
+function pageController()
+{
+	$counter = isset($_GET['key']) ? $_GET['key']: 0;
+	
+
+
+
+	return array(
+		'counter' => $counter);
+		
+}
+extract(pageController());
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel='stylesheet' href="/css/css_pingpong.css">
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<h2>Ping:<?= $counter ?></h2>
+	<a class="PingPong" href="pong.php?key=<?=$counter+1?>">Hit</a>
+	<a class="PingPong" href="ping.php?key=<?=$counter=0?>">Miss</a>
+
+</body>
+</html>
---
+++ public/pong.php
@@ -0,0 +1,29 @@
+<?php
+function pageController()
+{
+	$counter = isset($_GET['key']) ? $_GET['key']: 0;
+	
+	// $up=$counter+1;
+	// $down=$counter-1;
+
+
+	return array(
+		'counter' => $counter);
+		
+}
+extract(pageController());
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel='stylesheet' href="/css/css_pingpong.css">
+</head>
+<body>
+	<h2>Pong:<?= $counter ?></h2>
+	<a class="PingPong" href="ping.php?key=<?=$counter+1?>">Hit</a>
+	<a class="PingPong" href="ping.php?key=<?=$counter=0?>">Miss</a>
+
+</body>
+</html>
